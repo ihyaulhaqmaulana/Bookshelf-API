85e6884edce08db29d3c4ad249e37eb3fc2dfffd
Ihyaulhaq Maulana <ihyaulhaqmaulana243@gmail.com>
2021-08-31 10:14:06 +0700

    fix bug

--- src/handler.js
+++ src/handler.js
@@ -1,44 +1,47 @@
-const books = require('./books');
 const { nanoid } = require('nanoid');
+const books = require('./books');
 
 const addBookHandler = (request, h) => {
   const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
 
   const id = nanoid(16);
-  let finished = (pageCount === readPage ? true : false);
+  const finished = pageCount === readPage;
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
 
   const newBook = {
-    name, year, author, summary, publisher, pageCount, readPage, reading, id, finished, insertedAt, updatedAt
+    id, name, year, author, summary, publisher, pageCount, readPage, finished, reading,  insertedAt, updatedAt
   };
 
-  books.push(newBook);
-
-  const isSuccess = books.filter(book => book.id === id).length > 0;
-  const nameNull = (name === undefined ? true : false);
-  const exceedPageCount = (readPage > pageCount ? true : false);
-
-  if(nameNull) {
+  
+  
+  if(name === undefined) {
     const response = h.response({
       status: 'fail',
       message: 'Gagal menambahkan buku. Mohon isi nama buku',
     });
     response.code(400);
     return response;
-  } else if (exceedPageCount) {
+  } 
+  
+  if (readPage > pageCount) {
     const response = h.response({
       status: 'fail',
       message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
     });
     response.code(400);
     return response;
-  } else if (isSuccess) {
+  } 
+  
+  books.push(newBook);
+  const isSuccess = books.filter(book => book.id === id).length > 0;
+  
+  if (isSuccess) {
     const response = h.response({
       status: 'success',
       message: 'Buku berhasil ditambahkan',
       data: {
-        bookId: id
+        bookId: id,
       }
     });
     response.code(201);
@@ -54,16 +57,16 @@ const addBookHandler = (request, h) => {
 };
 
 
-const getAllBooksHandler = (request, h) => {
-  const response = h.response({
+const getAllBooksHandler = () => ({
     status: 'success',
     data: {
-      books,
+      books: books.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+      })),
     },
   });
-  response.code(201);
-  return response;
-};
 
 const getBookByIdHandler = (request, h) => {
   const { bookId } = request.params;
